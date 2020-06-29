0f89e6f696e6fb8cbcac3ffc60a23d884282c5ea
Bryan Mulingbayan <bmulingbayan@yondu.com>
2020-06-29 16:27:47 +0800

    Update keys

--- src/view/content/investments/Investment.vue
+++ src/view/content/investments/Investment.vue
@@ -14,7 +14,7 @@
               </div>
               <div class="d-flex flex-column">
                 <p class="mb-2 font-weight-boldest">EARLY REPAYMENT DUE</p>
-                <p>{{ investment.repayment_due }}</p>
+                <p>{{ investment.loan.maturity_date }}</p>
               </div>
             </td>
             <td style="border-top: none" width="26%">
@@ -22,21 +22,21 @@
                 <p class="mb-2 font-weight-boldest">
                   INVOICES AVAILABLE FOR FUNDING
                 </p>
-                <p>{{ investment.available_funding_left }}</p>
+                <p>{{ investment.companyInfo.total_available_invoices }}</p>
               </div>
               <div class="d-flex flex-column">
                 <p class="mb-2 font-weight-boldest">RETURNS</p>
-                <p>{{ investment.returns }}</p>
+                <p>{{ investment.loan.interest_rate }}</p>
               </div>
             </td>
             <td style="border-top: none" width="23%">
               <div class="d-flex flex-column">
                 <p class="mb-2 font-weight-boldest">TOTAL INVOICE AMOUNT</p>
-                <p>{{ investment.total_invoce_amount }}</p>
+                <p>{{ investment.total_funded }}</p>
               </div>
               <div class="d-flex flex-column">
                 <p class="mb-2 font-weight-boldest">TENURE</p>
-                <p>{{ investment.tenure }}</p>
+                <p>{{ investment.loan.tenure_label }}</p>
               </div>
             </td>
             <td style="border-top: none" width="16%" class="align-middle">
