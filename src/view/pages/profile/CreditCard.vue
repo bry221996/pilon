09f523a4fc094c4ec408471edb816c74aec4d4b5
bmulingbayan <bryan@coderinteractive.com>
2020-06-30 09:33:01 +0800

    WIP: Profile page

---
+++ src/view/pages/profile/CreditCard.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="row">
+		<div class="col-9">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-2">
+                        <inline-svg class="w-100" src="media/pilons/pilon_cc.svg"></inline-svg>
+                    </div>
+					<div class="col-10">ss</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-3">
+			<inline-svg class="w-100" src="media/pilons/pilon_cc_bg.svg"></inline-svg>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "CreditCard"
+};
+</script>
---
+++ src/view/pages/profile/Index.vue
@@ -0,0 +1,55 @@
+<template>
+	<div class="my-10 container-fluid">
+		<div class="row">
+			<div class="col-12">
+				<div class="links">
+					<router-link
+						v-for="(link, i) in links"
+						:key="i"
+						:to="link.to"
+						class="btn btn-sm rounded-pill mr-3 px-5 font-weight-boldest"
+						exact-active-class="btn-info"
+					>{{ link.display }}</router-link>
+				</div>
+			</div>
+		</div>
+
+		<div class="row my-5">
+			<div class="col-12">
+				<div class="p-5" style="background-color: #F2F3FF">
+					<p class="text-primary mb-0  font-weight-boldest text-uppercase">{{ headerTitle }}</p>
+				</div>
+			</div>
+		</div>
+
+		<transition name="fade-in-up">
+			<router-view></router-view>
+		</transition>
+	</div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+	name: "Investments",
+	data() {
+		return {
+			links: [
+				{ display: "PERSONAL DETAILS", to: "/profile/personal-details" },
+				{ display: "COMANPY DETAILS", to: "/profile/company-details" },
+				{ display: "CREDIT CARD", to: "/profile/credit-card" },
+				{ display: "BANK ACCOUNT", to: "/profile/bank-account" }
+			]
+		};
+	},
+	computed: {
+		...mapState({
+			funds: state => state.auth.user.fundSummary
+        }),
+        headerTitle() {
+            return this.$route.name.replace('-', ' ');
+        }
+	}
+};
+</script>
